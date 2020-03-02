484a677af7eff382ed0999e42db0777dc7b52220
giulio.costantini@unimib.it <giulio.costantini@unimib.it>
2020-03-02 17:38:00 +0100

    fixed bug in markerIndex which returned absolute loadings with orthogonal rotation

--- R/markerIndex.R
+++ R/markerIndex.R
@@ -3,7 +3,7 @@ markerIndex <- function(x, rotation = c("orthogonal", "oblique"), Phi,
 {
   rotation <- match.arg(rotation)
   type.loading <- match.arg(type.loading)
-  
+
   if(any(c("fa", "principal") %in% class(x)))
   {
     # determine rotation (orthogonal or oblique), loading matrix, and factor correlation matrix Phi
@@ -14,7 +14,7 @@ markerIndex <- function(x, rotation = c("orthogonal", "oblique"), Phi,
       # extract loading matrix
       ld <- loadings(x)
       class(ld) <- "matrix"
-      
+
     } else
       rotation <- "oblique"
     # extract structure loading matrix
@@ -22,7 +22,7 @@ markerIndex <- function(x, rotation = c("orthogonal", "oblique"), Phi,
     class(ld) <- "matrix"
     pattern <- loadings(x)
     class(pattern) <- "matrix"
-    
+
   } else
   {
     if(missing(rotation))
@@ -33,7 +33,7 @@ markerIndex <- function(x, rotation = c("orthogonal", "oblique"), Phi,
     {
       stop("A Phi matrix of factor correlations needs to be specified")
     }
-    
+
     # determine loadings
     if(rotation == "orthogonal")
     {
@@ -53,7 +53,7 @@ markerIndex <- function(x, rotation = c("orthogonal", "oblique"), Phi,
 }
 
 if(rotation == "orthogonal") Phi <- diag(ncol(ld))
-
+ld_bkp <- ld
 ld <- abs(ld)
 
 # create empty marker index object
@@ -64,7 +64,7 @@ if(rotation == "orthogonal")
 {
   # for(i in 1:ncol(ld))
   #   mi[,i] <- 1-sqrt((1-ld[,i])^2 + rowSums(ld[,-i]^2))
-  
+
   # alternative formulation
   for(i in 1:ncol(ld))
   {
@@ -73,7 +73,7 @@ if(rotation == "orthogonal")
     fk[,-i] <- 0
     mi[,i] <- 1-sqrt(rowSums((fk - ld)^2))
   }
-  
+
 } else if(rotation == "oblique")
 {
   # for(i in 1:ncol(ld))
@@ -84,7 +84,7 @@ if(rotation == "orthogonal")
   #   fk[,-i] <- 0
   #   mi[,i] <- 1 - apply(((fk-ld) %*% Phi) * (fk-ld), 1, sum)
   # }
-  
+
   #alternative formulation
   for(k in 1:ncol(ld))
     for(i in 1:nrow(ld))
@@ -95,11 +95,11 @@ if(rotation == "orthogonal")
       fk[-k] <- 0
       mi[i, k] <- 1- sqrt(t(fk-ai) %*% Phi %*% (fk-ai))
     }
-  
+
 }
 
 if(rotation == "orthogonal")
-  loading <- ld
+  loading <- ld_bkp
 else if (rotation == "oblique")
   loading <- pattern
 
